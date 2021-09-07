a3f187572121bcda0085f9b6b183dffb8f91573c
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-07 19:29:56 -0400

    revisions added for tech to product shift

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,71 +6,78 @@ import Seo from "../components/seo"
 const ResumePage = () => {
   return (
     <div>
+      
       <Seo
         title="Jacob Stephens Resume"
         description="Jacob Stephens Resume"
         image="../images/stewardgoods-icon.png"
         pathname="/"
       />
-      <div class="header ">
-          <h1 id="jacob-stephens">Jacob Stephens</h1>
-          <p><a href="tel:4843567773">484-356-7773</a> | <a href="mailto:jacob@stewardgoods.com">jacob@stewardgoods.com</a> | <a href="http://jacobstephens.net">jacobstephens.net</a></p>
+
+      <div class="header">
+        <h1 id='jacob-stephens'>Jacob Stephens</h1>
+        <p><a href='https://stewardgoods.com'>stewardgoods.com</a> | <a href='mailto:jacob@stewardgoods.com/'>jacob@stewardgoods.com</a> | <a href='tel:484-356-7773'>484-356-7773</a></p>
       </div>
 
       <div class="content">
-          <h2 id="work">Work</h2>
-          <h3 id="web-designer-and-graphic-designer">Web Designer and Graphic Designer</h3>
-          <h4 id="goodness-creative--sep-19---present">Goodness Creative | <div class="date">Sep '19 - Present</div></h4>
-          <ul>
-              <li>Rebuilt Chester County Life's website as a WordPress site, <a class="nowrap" href="http://chestercounty-life.com">chestercounty-life.com</a></li>
-              <li>Designed custom WordPress theme for&nbsp;<a href="http://chestercounty-life.com">chestercounty-life.com</a> using&nbsp;WP&nbsp;Rig</li>
-              <li>Created a WordPress site for Goodness Creative,&nbsp;<a href="http://goodness-creative.com">goodness-creative.com</a></li>
-              <li>Added content to pages on WordPress site&nbsp;<a href="http://biomasscontrols.com">biomasscontrols.com</a></li>
-              <li>Tested updates and maintenance to <a href="http://brandywineoak.com">brandywineoak.com</a> in development environment before making changes&nbsp;live</li>
-              <li>Developing site with React.js which is scoring 100/100 for Accessibility and Best Practices on Lighthouse 7.5.0&nbsp;(<a href="http://coachscall.gatsbyjs.io">coachscall.gatsbyjs.io</a>)</li>
-              <li>Restyled pages on the WordPress site with CSS,&nbsp;<a href="http://brandywineoak.com">brandywineoak.com</a></li>
-              <li>Taught Chester County Life how to navigate the Google Analytics connected to their&nbsp;site</li>
-          </ul>
-          <h3 id="web-developer-and-founder">Web Developer and Founder</h3>
-          <h4 id="steward-goods-web-design--may-18---present">Steward Goods Web Design | <div class="date">May '18 - Present</div></h4>
-          <ul>
-              <li>Raised Lighthouse 7.5.0 performance score from 57/100 to 100/100 and SEO score from 89/100 to 100/100 in rebuild of WordPress site <a href="http://stewardgoods.com">stewardgoods.com</a> to headless WP Gatsby.js site&nbsp;<a href="http://jacobstephens.net">jacobstephens.net</a></li>
-              <li>Transformed <a href="http://jacobstephens.net">jacobstephens.net</a> into a progressive web app making it installable and PWA optimized</li>
-              <li>Made custom content management system by writing 4,215 lines of HTML, CSS, JavaScript, PHP, and SQL, 350 comments, 28 tables, and 19,583 records to implement Minimalists inspired use of artifacts&nbsp;(<a href="http://artifacts.stewardgoods.com">artifacts.stewardgoods.com</a>)</li>
-              <li>Built e-commerce functionality into a previous version of <a href="http://stewardgoods.com">stewardgoods.com</a> and used to transact some of the over $35,000 of board games sold through&nbsp;Steward&nbsp;Goods</li>
-          </ul>
-          <h3 id="computer-support-analyst">Computer Support Analyst</h3>
-          <h4 id="alvernia-university--may-18--nov-18">Alvernia University | <div class="date">May '18 – Nov '18</div></h4>
-          <h3 id="computer-technician">Computer Technician</h3>
-          <h4 id="kingdom-supply--jan-18--sep-18">Kingdom Supply | <div class="date">Jan '18 – Sep '18</div></h4>
-          <h3 id="sales-associate">Sales Associate</h3>
-          <h4 id="weavers-orchard--june-10--aug-13-jul-16--nov-16">Weaver’s Orchard | <div class="date">June '10 – Aug '13; Jul '16 – Nov '16</div></h4>
-          <hr />
-          <h2 id="education">Education</h2>
-          <h3 id="certified-advanced-wordpress-developer">Certified Advanced WordPress&nbsp;Developer</h3>
-          <h4 id="linkedin-learning--apr-20---apr-21">LinkedIn Learning | <div class="date">Apr '20 - Apr '21</div></h4>
-          <h3 id="comptia-a-computer-technician">CompTIA A+ Computer Technician</h3>
-          <h4 id="berks-technical-institute--aug-17---nov-17">Berks Technical Institute | <div class="date">Aug '17 - Nov '17</div></h4>
-          <h3 id="bachelor-of-arts-with-a-major-in-sociology">Bachelor of Arts with a Major in&nbsp;Sociology</h3>
-          <h4 id="gordon-college--aug-12---aug-17">Gordon College | <div class="date">Aug '12 - Aug '17</div></h4>
-          <ul class="college_list">
-              <li><strong>Program Aide</strong><br />European Center for the Study of War and Peace | <div class="date">Sep '16 - Dec '16</div></li>
-              <li><strong>Honors Student</strong><br />Elijah Project for Work and Vocation | <div class="date">Jan '15 - May '16</div></li>
-              <li><strong>Delegate for Libya</strong><br />Harvard National Model United Nations | <div class="date">Jan '15 - Apr '15</div></li>
-              <li><strong>Administrative Assistant</strong><br />Philabundance Community Kitchen | <div class="date">May '15 – Aug '15</div></li>
-              <li><strong>Research Assistant</strong><br />Agora Institute for Civic Virtue and the Common Good | <div class="date">May '15 – Sep '15</div></li>
-              <li><strong>Event Management Apprentice</strong><br />Center for Faith and Inquiry | <div class="date">Sep '14 – May '15</div></li>
-              <li><strong>Field Intern</strong><br />Partners in Development | <div class="date">Jun '14 – Aug '14</div></li>
-              <li><strong>Companion to Residents with Dementia</strong><br />Essex Park Rehabilitation Center | <div class="date">Jan '14 - May '14</div></li>
-              <li><strong>Resident Advisor</strong><br />Gordon College Residence Life | <div class="date">Aug '13 – May '14</div></li>
-              <li><strong>Childcare Worker</strong><br />The Boys and Girls Club of Lynn | <div class="date">Jan '13 - May '13</div></li>
-          </ul>
-          <hr />
-          <h2 id="volunteering-and-leisure">Volunteering and Leisure</h2>
-          <h3 id="board-game-event-cohost">Board Game Event&nbsp;Cohost</h3>
-          <h4 id="meetup--feb-18---present">Meetup | Feb '18 - Present</h4>
-          <h3 id="beekeeper">Beekeeper</h3>
-          <h4 id="steward-goods-honey--mar-17---sep-20">Steward Goods Honey | Mar '17 - Sep '20</h4>
+        <h2 id='work'>Work</h2>
+        <h3 id='web-designer-and-graphic-designer'>Web Designer and Graphic Designer</h3>
+        <h4 id='15-east-creative--sep-19---present'>15 East Creative | Sep &#39;19 - Present</h4>
+        <ul>
+        <li>Developed <a href='https://dana4wvt.com/'>dana4wvt.com</a> from scratch for Dana Alan&#39;s campaign for West Vincent Township supervisor, expanding accessibility by making it a progressive web app, installable and PWA optimized</li>
+        <li>Restyled <a href='https://chestercounty-life.com'>chestercounty-life.com</a> to better match the aesthetic of Chester County Life&#39;s latest magazines</li>
+        <li>Added subscription functionality to <a href='https://chestercounty-life.com'>chestercounty-life.com</a> which meets the Payment Card Industry data security standard </li>
+        <li>Creating a new website for 15 East Creative at <a href='https://15east.gatsbyjs.io'>15east.gatsbyjs.io</a></li>
+        <li>Built a parralax effect into a sample homepage for Goodness Creative: <a href='https://goodnesscreative.gatsbyjs.io'>goodnesscreative.gatsbyjs.io</a></li>
+        <li>Added content to <a href='https://biomasscontrols.com'>biomasscontrols.com</a>, matching the style of the new content with the existing content</li>
+        <li>Improved appearance of <a href='https://brandywineoak.com'>brandywineoak.com</a>, making text more readable and breakpoint changes more pleasing</li>
+        <li>Developing a site for Coach&#39;s Call, an athletic mentoring program, at <a href='https://coachscall.gatsbyjs.io/'>coachscall.gatsbyjs.io</a></li>
+        <li>Taught Chester County Life how to navigate the Google Analytics I installed on their website</li>
+
+        </ul>
+        <h3 id='web-developer-and-founder'>Web Developer and Founder</h3>
+        <h4 id='steward-goods-web-design--may-18---present'>Steward Goods Web Design | May &#39;18 - Present</h4>
+        <ul>
+        <li>Created the single page information site <a href='https://klsweddingcoordination.gatsbyjs.io'>klsweddingcoordination.gatsbyjs.io</a> for KLS Wedding Coordination, adopting best practices in development</li>
+        <li>Engineered <a href='https://artifacts.stewardgoods.com'>artifacts.stewardgoods.com</a>, a tool empowering people to track uses of their artifacts and generate use-by date lists for those artifacts</li>
+        <li>Increased the performance of <a href='https://stewardgoods.com/'>stewardgoods.com</a> by transitioning it to a Gatsby static generated site with WordPress as the content management system</li>
+        <li>Upgraded search engine optimization of <a href='https://jacobcstephens.com/'>jacobcstephens.com</a>, and in so doing attained a perfect score in all categories on Google&#39;s Core Web Vitals test with Lighthouse 8.3.0 on Sep. 7, 2021</li>
+        <li>Produced <a href='https://roseriverway.com'>roseriverway.com</a> and taught the site owner how to make their own updates to the site</li>
+
+        </ul>
+        <h3 id='computer-support-analyst'>Computer Support Analyst</h3>
+        <h4 id='alvernia-university--may-18----nov-18'>Alvernia University | May &#39;18 – Nov &#39;18</h4>
+        <h3 id='computer-technician'>Computer Technician</h3>
+        <h4 id='kingdom-supply--jan-18----sep-18'>Kingdom Supply | Jan &#39;18 – Sep &#39;18</h4>
+        <h3 id='sales-associate'>Sales Associate</h3>
+        <h4 id='weavers-orchard--june-10----aug-13-jul-16----nov-16'>Weaver’s Orchard | June &#39;10 – Aug &#39;13; Jul &#39;16 – Nov &#39;16</h4>
+        <hr />
+        <h2 id='education'>Education</h2>
+        <h3 id='certified-advanced-wordpress-developer'>Certified Advanced WordPress Developer</h3>
+        <h4 id='linkedin-learning--apr-20---apr-21'>LinkedIn Learning | Apr &#39;20 - Apr &#39;21</h4>
+        <h3 id='comptia-a-computer-technician'>CompTIA A+ Computer Technician</h3>
+        <h4 id='berks-technical-institute--aug-17---nov-17'>Berks Technical Institute | Aug &#39;17 - Nov &#39;17</h4>
+        <h3 id='bachelor-of-arts-with-a-major-in-sociology'>Bachelor of Arts with a Major in Sociology</h3>
+        <h4 id='gordon-college--aug-12---aug-17'>Gordon College | Aug &#39;12 - Aug &#39;17</h4>
+        <ul>
+        <li><strong>Program Aide</strong> | European Center for the Study of War and Peace | Sep &#39;16 - Dec &#39;16</li>
+        <li><strong>Honors Student</strong> | Elijah Project for Work and Vocation | Jan &#39;15 - May &#39;16</li>
+        <li><strong>Delegate for Libya</strong> | Harvard National Model United Nations | Jan &#39;15 - Apr &#39;15</li>
+        <li><strong>Administrative Assistant</strong> | Philabundance Community Kitchen | May &#39;15 – Aug &#39;15</li>
+        <li><strong>Research Assistant</strong> | Agora Institute for Civic Virtue and the Common Good | May &#39;15 – Sep &#39;15</li>
+        <li><strong>Event Management Apprentice</strong> | Center for Faith and Inquiry | Sep &#39;14 – May &#39;15</li>
+        <li><strong>Field Intern</strong> | Partners in Development | Jun &#39;14 – Aug &#39;14</li>
+        <li><strong>Companion to Residents with Dementia</strong> | Essex Park Rehabilitation Center | Jan &#39;14 - May &#39;14</li>
+        <li><strong>Resident Advisor</strong> | Gordon College Residence Life | Aug &#39;13 – May &#39;14</li>
+        <li><strong>Childcare Worker</strong> | The Boys and Girls Club of Lynn | Jan &#39;13 - May &#39;13</li>
+
+        </ul>
+        <hr />
+        <h2 id='volunteering-and-leisure'>Volunteering and Leisure</h2>
+        <h3 id='board-game-event-cohost'>Board Game Event Cohost</h3>
+        <h4 id='meetup--feb-18---present'>Meetup | Feb &#39;18 - Present</h4>
+        <h3 id='beekeeper'>Beekeeper</h3>
+        <h4 id='steward-goods-honey--mar-17---sep-20'>Steward Goods Honey | Mar &#39;17 - Sep &#39;20</h4>
       </div>
     </div>
   )
