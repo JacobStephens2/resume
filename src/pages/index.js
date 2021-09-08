9168d5fa567419a30ef8cd28f6ad58281ab318d7
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-07 22:03:12 -0400

    style improvement

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 const ResumePage = () => {
   return (
     <div>
-      
+
       <Seo
         title="Jacob Stephens Resume"
         description="Jacob Stephens Resume"
@@ -21,28 +21,28 @@ const ResumePage = () => {
 
       <div class="content">
         <h2 id='work'>Work</h2>
-        <h3 id='web-designer-and-graphic-designer'>Web Designer and Graphic Designer</h3>
+        <h3 id='web-designer-and-graphic-designer'>Web Designer and Graphic&nbsp;Designer</h3>
         <h4 id='15-east-creative--sep-19---present'>15 East Creative | Sep &#39;19 - Present</h4>
         <ul>
-        <li>Developed <a href='https://dana4wvt.com/'>dana4wvt.com</a> from scratch for Dana Alan&#39;s campaign for West Vincent Township supervisor, expanding accessibility by making it a progressive web app, installable and PWA optimized</li>
-        <li>Restyled <a href='https://chestercounty-life.com'>chestercounty-life.com</a> to better match the aesthetic of Chester County Life&#39;s latest magazines</li>
-        <li>Added subscription functionality to <a href='https://chestercounty-life.com'>chestercounty-life.com</a> which meets the Payment Card Industry data security standard </li>
-        <li>Creating a new website for 15 East Creative at <a href='https://15east.gatsbyjs.io'>15east.gatsbyjs.io</a></li>
-        <li>Built a parralax effect into a sample homepage for Goodness Creative: <a href='https://goodnesscreative.gatsbyjs.io'>goodnesscreative.gatsbyjs.io</a></li>
-        <li>Added content to <a href='https://biomasscontrols.com'>biomasscontrols.com</a>, matching the style of the new content with the existing content</li>
-        <li>Improved appearance of <a href='https://brandywineoak.com'>brandywineoak.com</a>, making text more readable and breakpoint changes more pleasing</li>
-        <li>Developing a site for Coach&#39;s Call, an athletic mentoring program, at <a href='https://coachscall.gatsbyjs.io/'>coachscall.gatsbyjs.io</a></li>
-        <li>Taught Chester County Life how to navigate the Google Analytics I installed on their website</li>
+        <li>Developed <a href='https://dana4wvt.com/'>dana4wvt.com</a> from scratch for Dana Alan&#39;s campaign for West Vincent Township supervisor, expanding accessibility by making it a progressive web app, installable and PWA&nbsp;optimized</li>
+        <li>Restyled <a href='https://chestercounty-life.com'>chestercounty-life.com</a> to better match the aesthetic of Chester County Life&#39;s latest&nbsp;magazines</li>
+        <li>Added subscription functionality to <a href='https://chestercounty-life.com'>chestercounty-life.com</a> which meets the Payment Card Industry data security&nbsp;standard </li>
+        <li>Creating a new website for 15 East Creative at&nbsp;<a href='https://15east.gatsbyjs.io'>15east.gatsbyjs.io</a></li>
+        <li>Built a parralax effect into a sample homepage for Goodness&nbsp;Creative:&nbsp;<a href='https://goodnesscreative.gatsbyjs.io'>goodnesscreative.gatsbyjs.io</a></li>
+        <li>Added content to <a href='https://biomasscontrols.com'>biomasscontrols.com</a>, matching the style of the new content with the existing&nbsp;content</li>
+        <li>Improved appearance of <a href='https://brandywineoak.com'>brandywineoak.com</a>, making text more readable and breakpoint changes more&nbsp;pleasing</li>
+        <li>Developing a site for Coach&#39;s Call, an athletic mentoring program, at&nbsp;<a href='https://coachscall.gatsbyjs.io/'>coachscall.gatsbyjs.io</a></li>
+        <li>Taught Chester County Life how to navigate the Google Analytics I installed on their&nbsp;website</li>
 
         </ul>
-        <h3 id='web-developer-and-founder'>Web Developer and Founder</h3>
+        <h3 id='web-developer-and-founder'>Web Developer and&nbsp;Founder</h3>
         <h4 id='steward-goods-web-design--may-18---present'>Steward Goods Web Design | May &#39;18 - Present</h4>
         <ul>
-        <li>Created the single page information site <a href='https://klsweddingcoordination.gatsbyjs.io'>klsweddingcoordination.gatsbyjs.io</a> for KLS Wedding Coordination, adopting best practices in development</li>
-        <li>Engineered <a href='https://artifacts.stewardgoods.com'>artifacts.stewardgoods.com</a>, a tool empowering people to track uses of their artifacts and generate use-by date lists for those artifacts</li>
-        <li>Increased the performance of <a href='https://stewardgoods.com/'>stewardgoods.com</a> by transitioning it to a Gatsby static generated site with WordPress as the content management system</li>
-        <li>Upgraded search engine optimization of <a href='https://jacobcstephens.com/'>jacobcstephens.com</a>, and in so doing attained a perfect score in all categories on Google&#39;s Core Web Vitals test with Lighthouse 8.3.0 on Sep. 7, 2021</li>
-        <li>Produced <a href='https://roseriverway.com'>roseriverway.com</a> and taught the site owner how to make their own updates to the site</li>
+        <li>Created the single page information site <a href='https://klsweddingcoordination.gatsbyjs.io'>klsweddingcoordination.gatsbyjs.io</a> for KLS Wedding Coordination, adopting best practices in&nbsp;development</li>
+        <li>Engineered <a href='https://artifacts.stewardgoods.com'>artifacts.stewardgoods.com</a>, a tool empowering people to track uses of their artifacts and generate use-by date lists for those&nbsp;artifacts</li>
+        <li>Increased the performance of <a href='https://stewardgoods.com/'>stewardgoods.com</a> by transitioning it to a Gatsby static generated site with WordPress as the content management&nbsp;system</li>
+        <li>Upgraded search engine optimization of <a href='https://jacobcstephens.com/'>jacobcstephens.com</a>, and in so doing attained a perfect score in all categories on Google&#39;s Core Web Vitals test with Lighthouse&nbsp;8.3.0</li>
+        <li>Produced <a href='https://roseriverway.com'>roseriverway.com</a> and taught the site owner how to make their own updates to the&nbsp;site</li>
 
         </ul>
         <h3 id='computer-support-analyst'>Computer Support Analyst</h3>
@@ -74,7 +74,7 @@ const ResumePage = () => {
         </ul>
         <hr />
         <h2 id='volunteering-and-leisure'>Volunteering and Leisure</h2>
-        <h3 id='board-game-event-cohost'>Board Game Event Cohost</h3>
+        <h3 id='board-game-event-cohost'>Board Game Event&nbsp;Cohost</h3>
         <h4 id='meetup--feb-18---present'>Meetup | Feb &#39;18 - Present</h4>
         <h3 id='beekeeper'>Beekeeper</h3>
         <h4 id='steward-goods-honey--mar-17---sep-20'>Steward Goods Honey | Mar &#39;17 - Sep &#39;20</h4>
