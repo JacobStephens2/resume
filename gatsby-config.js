a5ce4aaea35fbb12d052ad51b275bc5004cd443e
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-10-01 18:36:21 -0400

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,5 +48,24 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-90JQWVG3TL", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ],
 }
