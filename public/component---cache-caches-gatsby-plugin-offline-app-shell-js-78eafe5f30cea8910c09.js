9d3aff730fe1c0b94d4d5dd073509d56bc5c7d43
Jacob Stephens <jacob@stewardgoods.com>
2021-10-14 16:13:15 -0400

    add CSS Remedy

--- public/component---cache-caches-gatsby-plugin-offline-app-shell-js-78eafe5f30cea8910c09.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkjacobstephensresume=self.webpackChunkjacobstephensresume||[]).push([[306],{1930:function(e,t,n){"use strict";var u=n(5318);t.__esModule=!0,t.default=void 0;var r=u(n(5354)),a=u(n(7294)),s=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},t}(a.default.Component);t.default=s}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-78eafe5f30cea8910c09.js.map
--- public/component---src-pages-404-js-c42a166da12069295d4c.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkjacobstephensresume=self.webpackChunkjacobstephensresume||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var u=n(7294),s=n(3751);t.default=function(){return u.createElement("div",null,u.createElement(s.Z,{title:"404: Not found"}),u.createElement("h1",null,"Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-c42a166da12069295d4c.js.map
