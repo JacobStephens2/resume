069874afee7c05dd71f439744cdd276d7ee55f04
Jacob Stephens <jacob@stewardgoods.com>
2022-02-22 21:20:13 -0500

    add ETA position

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,28 +22,61 @@ const ResumePage = () => {
            | 
           <a href="tel:484-356-7773">484.356.7773</a> | Lancaster,&nbsp;PA
         </p>
+
         <h2>Work</h2>
-        <h3><a href="https://www.dropbox.com/s/5bmnotwzas2zco0/Web%20Programmer_Developer%20-%20Conestoga%2C%20PA%20-%20Indeed.com.pdf?dl=0">Web Developer and Client Support Specialist</a></h3>
+
+        <h3>
+          <a href="https://www.dropbox.com/s/97nrrwwiupiuee8/Offer_Letter_Jacob-public.pdf?dl=0">
+            Full Stack Web Developer
+          </a>
+        </h3>
         <p>
-          <a href="https://sharpinnovations.com/">Sharp Innovations</a> | Nov 2021 –
-          Present |{" "}
+          <a href="https://etadventures.com/">Educational Travel Adventures</a>{" "}
+          | Feb 2022 – Present |{" "}
+          <a href="https://goo.gl/maps/Td5mBiT62q6QarPn6">Remote</a>
+        </p>
+        <ul>
+          <li>
+            Leading technical development and support of customer facing
+            websites, web applications, and mobile / hybrid applications
+          </li>
+          <li>
+            Planning, designing, and implementing a variety of technical
+            solutions across the business
+          </li>
+          <li>Owning projects from inception through completion</li>
+        </ul>
+
+        <h3>
+          <a href="https://www.dropbox.com/s/5bmnotwzas2zco0/Web%20Programmer_Developer%20-%20Conestoga%2C%20PA%20-%20Indeed.com.pdf?dl=0">
+            Web Developer and Client Support Specialist
+          </a>
+        </h3>
+        <p>
+          <a href="https://sharpinnovations.com/">Sharp Innovations</a> | Nov
+          2021 – Feb 2022 |{" "}
           <a href="https://g.page/SharpInnovationsInc?share">Conestoga, PA</a>
         </p>
         <ul>
           <li>
-            Developed a method for database migration which cut the process from hours to&nbsp;minutes using SQL and PHP
+            Developed a method for database migration which cut the process from
+            hours to&nbsp;minutes using SQL and PHP
           </li>
           <li>
-            Supported web customers ranging from aesthetic site revisions to security patches to Linux command line email&nbsp;troubleshooting
+            Supported web customers ranging from aesthetic site revisions to
+            security patches to Linux command line email&nbsp;troubleshooting
           </li>
           <li>
-            Structured team work process by creating a task board to organize work more effectively than the previous email inbox task management&nbsp;system
+            Structured team work process by creating a task board to organize
+            work more effectively than the previous email inbox task
+            management&nbsp;system
           </li>
         </ul>
+
         <h3>Full-Stack Web Developer</h3>
         <p>
-          <a href="https://stewardgoods.com/">Steward Goods</a> | Mar 2020 –
-          Nov 2021 |{" "}
+          <a href="https://stewardgoods.com/">Steward Goods</a> | Mar 2020 – Nov
+          2021 |{" "}
           <a href="https://goo.gl/maps/SPNKz1YgRcM9ZoWEA">Lancaster, PA</a>
         </p>
         <ul>
@@ -54,42 +87,26 @@ const ResumePage = () => {
           <li>
             <a href="https://stewardgoods.com">Optimized</a> sites, attaining
             perfect scores on Google's Core Web Vitals Lighthouse
-            8.5.1&nbsp;tests
+            8.5.1&nbsp;tests, including headless WordPress sites
           </li>
           <li>
             <a href="https://chestercounty-life.com">Implemented</a> PCI
             compliant subscription functionality on website with&nbsp;Stripe
           </li>
           <li>
-            <a href="https://goodnesscreative.gatsbyjs.io">Refined</a> a
-            parallax effect in web design brainstorming with&nbsp;SASS
+            <a href="https://artifact-minimalism.site/">Engineered</a> a content
+            management system using PHP and&nbsp;MySQL 
           </li>
           <li>
-            <a href="https://biomasscontrols.com">Expanded</a> pages, sections,
-            and content on sites
-          </li>
-          <li>
-            <a href="https://brandywineoak.com">Improved</a> text readability of
-            web apps by following WCAG
-          </li>
-          <li>
-            <a href="https://artifacts.stewardgoods.com">Engineered</a> a
-            content management system using PHP and&nbsp;MySQL 
-          </li>
-          <li>
-            <a href="https://jacobcstephens.com">Increased</a> the performance
-            of a WordPress site 60% by making it headless with GraphQL API
-            and&nbsp;Gatsby.js
-          </li>
-          <li>
-            <a href="https://15eastcreative.com">Creating</a> web app to match
+            <a href="https://15eastcreative.com">Created</a> web app to match
             company style&nbsp;guide
           </li>
           <li>
-            <a href="https://coachscall.gatsbyjs.io">Produced</a> a progressive
-            web app for an athletic mentoring program
+            <a href="https://coachscall.org">Produced</a> a progressive web app
+            for an athletic mentoring program
           </li>
         </ul>
+
         <h3 id="graphic-designer">Graphic Designer</h3>
         <p>
           <a href="https://stewardgoods.com/">Steward Goods</a> | Sep 2019 –
@@ -114,14 +131,8 @@ const ResumePage = () => {
             <a href="https://stewardgoods.com/montgomery-school-map/">Drew</a>{" "}
             illustrations with Illustrator, making spaces more&nbsp;navigable
           </li>
-          <li>
-            <a href="https://chestercounty-life.com/past-issues/#pdf-chester-county-life-september-october-2021/1/">
-              Designed
-            </a>{" "}
-            products according to style guides, keeping branding consistent
-            through&nbsp;materials
-          </li>
         </ul>
+
         <h3>Ecommerce Business Owner</h3>
         <p>
           <a href="https://stewardgoods.com/">Steward Goods</a> | May 2018 – Mar
@@ -137,6 +148,7 @@ const ResumePage = () => {
             sales, plus more in other&nbsp;categories
           </li>
         </ul>
+
         <h3>Computer Support Analyst</h3>
         <p>
           <a href="https://www.alvernia.edu/">Alvernia University</a> | May 2018
@@ -148,6 +160,7 @@ const ResumePage = () => {
           <li>Maintained campus computers through&nbsp;re-imaging</li>
           <li>Managed technology&nbsp;inventory</li>
         </ul>
+
         <h3>Computer Technician</h3>
         <p>
           <a href="https://www.ksupplyco.com/">Kingdom Supply</a> | Jan 2018 –
@@ -163,39 +176,8 @@ const ResumePage = () => {
             Created imaging process to speed up new employee computer&nbsp;setup
           </li>
         </ul>
-        <h3>Sales Associate</h3>
-        <p>
-          <a href="https://www.weaversorchard.com/">
-            Weaver's Orchard Farm Market
-          </a>{" "}
-          | Jun 2010 – Nov 2016 |{" "}
-          <a href="https://goo.gl/maps/Axndxy6UZsA7HTny7">Morgantown, PA</a>
-        </p>
-        <ul>
-          <li>
-            Created data driven system to aid in market stand
-            product&nbsp;selection
-          </li>
-          <li>Sold produce and cared for plants</li>
-          <li>Reorganized storage to increase ease of&nbsp;use</li>
-        </ul>
-        <h2>Education</h2>
 
-        <h3>
-          <a href="https://www.linkedin.com/learning/paths/prepare-for-the-aws-certified-developer-associate-exam?u=95224929">
-            AWS Certified Developer - Associate
-          </a>
-        </h3>
-        <p>
-          <a href="https://aws.amazon.com/certification/certified-developer-associate/?ch=sec&sec=rmg&d=1">
-            Amazon Web Services
-          </a>{" "}
-          |{" "}
-          <a href="https://www.dropbox.com/sh/l13ewio70dv7puo/AABNuUvlvmaY_984uHm-ZnUYa?dl=0">
-            In Progress
-          </a>{" "}
-          | Remote
-        </p>
+        <h2>Education</h2>
 
         <h3>
           <a href="https://www.linkedin.com/learning/paths/become-a-full-stack-web-developer?u=95224929">
